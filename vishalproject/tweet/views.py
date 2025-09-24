864d048194a72c2514574a86f32612ac50ad5235
Vishalyadavroy <vishalyadavroy@gmail.com>
2025-09-25 03:48:33 +0530

    updates

--- vishalproject/tweet/views.py
+++ vishalproject/tweet/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from .models import Tweet
-from .forms import TweetForm
+from .models import Tweet 
+from .forms import TweetForm ,UserRegistrationForm
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+
+
 # Create your views here.
 def index(request):
       return render(request, 'index.html')
@@ -11,7 +15,7 @@ def tweet_list(request):
   tweets = Tweet.objects.all().order_by('-created_at')
   return render(request, 'tweet_list.html',{'tweets':tweets})
 
-
+@login_required
 def tweet_create(request):
    
     if request.method =="POST": #kya data aa rha hai 
@@ -25,6 +29,7 @@ def tweet_create(request):
         form =TweetForm()
     return render(request, 'tweet_form.html',{'form':form})
 
+@login_required
 def tweet_edit(request,tweet_id ):
     tweet = get_object_or_404(Tweet, pk=tweet_id , user=request.user)
     if request.method =='POST':
@@ -40,10 +45,27 @@ def tweet_edit(request,tweet_id ):
         form = TweetForm(instance = tweet)
     return render(request, 'tweet_form.html',{'form':form})
 
+@login_required
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
     if request.method == 'POST':
         tweet.delete()
         return redirect('tweet_list')
     return redirect('tweet_list')
+
+
+
+def register(request):
+    form = UserRegistrationForm()  # Initialize form here to avoid UnboundLocalError
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)  # Reinitialize the form if POST request
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            return redirect('tweet_list')
+    
+    return render(request, 'registration/register.html', {'form': form})
 
--- vishalproject/vishalproject/urls.py
+++ vishalproject/vishalproject/urls.py
@@ -18,8 +18,12 @@ from django.contrib import admin
 from django.urls import path ,include # ye isliye use karte hai django app lo inform kar chuke hai 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
